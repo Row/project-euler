3175c0fd4548028aa8819335046b984b382f6335
Jon <jon@powha.net>
2012-09-28 03:56:07 +0200

    Nightly

--- e012.erl
+++ e012.erl
@@ -1,29 +1,46 @@
 %,Problem,12
 -module(e012).
 -export([solve/0]).
-
-factors(N) ->
-	factors(N, N div 2, 1);
-	
-factors(N, 0, R) ->
-	io:format("~p ~p ~n", [R, N]),
-	R;
-factors(N, M, R) when N rem M =:= 0 ->
-	factors(N, M - 1, R + 1);
-factors(N, M, R) ->
-	factors(N, M - 1, R).
-
+-define(MAX, 500000000).
 
 solve() ->
-	solve(300000).
+	Primes = array:new([{size,?MAX},{fixed,true},{default,true}]),
+	P = gen_primes(Primes, 2, round(math:sqrt(?MAX-1)), ?MAX-1),
+	solve(P, ?MAX - 1).
 
-solve(N) ->
-	TriangleNumber = lists:sum(lists:seq(1, N)),
-	case factors(TriangleNumber) > 500 of
+solve(Primes, 0) ->
+	ok;
+solve(Primes, Index) ->
+	case array:get(Index, Primes) of
 		true ->
-			TriangleNumber;
+			io:format("~p~n", [Index]),
+			solve(Primes, Index - 1);
 		false ->
-			solve(N + 1)
+			solve(Primes, Index - 1)
 	end.
+
+%	TriangleNumber = lists:sum(lists:seq(1, N)),
+%	case TriangleNumber > 500 of
+%		true ->
+%			TriangleNumber;
+%		false ->
+%			solve(N + 1)
+%	end.
+
+
+gen_primes(Primes, Index, MaxSqrt, _Max) when Index > MaxSqrt ->
+	Primes;
+gen_primes(Primes, Index, MaxSqrt, Max) ->
+	case array:get(Index, Primes) of
+		true ->
+			gen_primes(gen_primes_inner(Primes, Index, Index, Max), Index+1, MaxSqrt, Max);
+		false ->
+			gen_primes(Primes, Index+1, MaxSqrt, Max)
+	end.
+
+gen_primes_inner(Primes, Index, IndexInner, Max) when Index * IndexInner >= Max ->
+	Primes;
+gen_primes_inner(Primes, Index, IndexInner, Max) ->
+	gen_primes_inner(array:set(Index * IndexInner, false, Primes), Index, IndexInner+1, Max).
 
 	
