ec64fa68933b00933c4906fb7b330c85babda273
Jon <jon@powha.net>
2012-10-02 01:14:57 +0200

    balls

--- e012.erl
+++ e012.erl
@@ -1,7 +1,7 @@
 %,Problem,12
 -module(e012).
 -export([solve/0, is_prime/1]).
--define(MAX, 10000).
+-define(MAX, 100000).
 
 is_prime(P) ->
     R = round(math:sqrt(P)),
@@ -14,32 +14,23 @@ is_prime(P, N) when P rem N =:= 0 ->
 is_prime(P, N) ->
     is_prime(P, N - 1).
 	
-factors(1) ->
-	[];
-factors(N) ->
-	case is_prime(N) of
-		true ->
-			[N];
-		false ->
-			factors(N, N - 1)
-	end.
+factors(PrimeCache, N) ->
+	factors(PrimeCache, N, N, []).
 
-factors(_N, 1) ->
-	[];
-factors(N, 2) ->
-	[2|factors(N div 2)];
-factors(N, D) when N rem 2 =:= 1 andalso N rem D =:= 0 ->
-	case is_prime(D) of
+factors(PrimeCache, _N, 1, Factors) ->
+	{PrimeCache, Factors};
+factors(PrimeCache, N, 2, Factors) -> 
+	factors(PrimeCache, N div 2, N div 2, [2|Factors]);
+factors(PrimeCache, N, D, Factors) when N rem 2 =:= 1 andalso N rem D =:= 0 ->
+	case array:get(D, PrimeCache) orelse is_prime(D) of
 		true ->
-	    	[D|factors(N div D)];
+	    	factors(array:set(D, true, PrimeCache), N div D, N div D, [D|Factors]);
 		false ->
-	    	factors(N, D - 1)
+	    	factors(PrimeCache, N, D - 1, Factors)
     end;
-factors(N, D) ->
-	factors(N, D - 1).
+factors(PrimeCache, N, D, Factors) ->
+	factors(PrimeCache, N, D - 1, Factors).
 
-
-	    
 triangle_num(X) ->
 	round(0.5 * X * (X + 1)).
 
@@ -65,20 +56,21 @@ product([H|T]) ->
 	H * product(T).
 
 solve() ->
-	A = array:new(),
+	A = array:new({default,false}),
     solve(A, ?MAX).
 
- solve(N) ->   
+ solve(PrimeCache, N) ->   
     TriangleNumber = triangle_num(N),
-    Factors = factors(TriangleNumber),
+    {PrimeCacheUp, Factors} = factors(PrimeCache, TriangleNumber),
     Divisors = product(count_pow(Factors)),
     p(Factors),
     io:format(" = ~p, n = ~p, divisors = ~p~n",[TriangleNumber, N, Divisors]),
+    %io:format("~p", [PrimeCacheUp]),
     case Divisors > 500 of
     	true ->
     		TriangleNumber;
 		false ->
-			solve(N + 1)
+			solve(PrimeCacheUp, N + 1)
 	end.
  
 p([H]) ->
