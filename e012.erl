12945191819d9a3f2ebdad09a2c0edee95d4a939
Jon <jon@powha.net>
2012-10-01 18:46:25 +0200

    Merge

--- e012.erl
+++ e012.erl
@@ -1,19 +1,17 @@
 %,Problem,12
 -module(e012).
 -export([solve/0]).
--define(MAX, 500000000).
+-define(MAX, 100000).
 
-solve() ->
-    Primes = array:new([{size,?MAX},{fixed,true},{default,true}]),
-    P = gen_primes(Primes, 2, round(math:sqrt(?MAX-1)), ?MAX-1),
-    solve(P, ?MAX - 1).
 
-factors(Primes, P, 1) ->
+
+factors(_Primes, 1, _N) ->
     [];
 factors(Primes, P, I) ->
-    case array:get(P, Primes) andalso P rem I =:= 0 of
+    %io:format("i: ~p~n",[I]),
+    case array:get(I, Primes) andalso P rem I =:= 0 of
 	true ->
-	    [I|factors(Primes, P div I, math:sqrt(P div I))];
+	    [I|factors(Primes, P div I, P div I)];
 	false ->
 	    factors(Primes, P, I - 1)
     end.
@@ -30,13 +28,29 @@ factors(Primes, P, I) ->
 %			solve(Primes, Index - 1);
 %		false ->
 %			solve(Primes, Index - 1)
+
 %	end.
+solve() ->
+    TriangleNumber = lists:sum(lists:seq(1, ?MAX+1)),
+    Primes = array:new([{size,TriangleNumber},{fixed,true},{default,true}]),
+    P = gen_primes(Primes, 2, math:sqrt(TriangleNumber), TriangleNumber),
+    io:format("~p",[P]),
+    solve(P, ?MAX - 1).
+
  solve(Primes, N) ->   
     TriangleNumber = lists:sum(lists:seq(1, N)),
-    Factors = factors(TriangleNumber, Trianglenumber),
-    io:format("~p~n",).
+    Factors = factors(Primes, TriangleNumber, TriangleNumber),
+    p(Factors),
+    io:format(" = ~p~n",[TriangleNumber]).
+ 
+p([H]) ->
+	io:format("~p", [H]);
+p([H|T]) ->
+	io:format("~p * ", [H]),
+	p(T).
+
  %   case factors(TriangleNumber) > 500 of
- %  	true ->
+ %  	true ->	
 %	    TriangleNumber;
 %	false ->
 %	    solve(N + 1)
