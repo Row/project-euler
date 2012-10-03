7618a4ee4974e9c0af086b28035d98530d69b541
Jon <jon.borglund@gmail.com>
2012-10-03 10:54:22 +0200

    amicable sum below N

--- e021.erl
+++ e021.erl
@@ -1,15 +1,14 @@
 -module(e021).
 -export([solve/0,d/1]).
-divisors(1) ->
-    [1];
+
 divisors(N) ->
     divisors(N, N - 1).
 divisors(_N, 1) ->
     [1];
 divisors(N, D) when N rem D =:= 0 ->
-    [D|divisors(N, D-1)];
+    [D|divisors(N, D - 1)];
 divisors(N, D) ->
-    divisors(N, D-1).
+    divisors(N, D - 1).
 
 proper_divisors(D, N) ->    
     [1|[X || X <- D, lists:member(N div X, D)]]. 
@@ -17,25 +16,27 @@ proper_divisors(D, N) ->
 d(N) ->
    lists:sum(proper_divisors(divisors(N), N)).
 
-amicable_pairs_below(_A, 0) ->
+amicable_pairs_below(N) ->
+    amicable_pairs_below(N-1, N).
+
+amicable_pairs_below(_A, 1) ->
     0;
-amicable_pairs_below(0, B) ->
-    amicable_pairs_below(B - 2, B - 1);
-amicable_pairs_below(A, B) when A =/= B ->
-    SA = d(A),
-    SB = d(B),
-    case A =:= SB andalso B =:= SA  of
-	true ->
-	    io:format("A = ~p, B = ~p~n",[A, B]),
-	    A + B + amicable_pairs_below(A - 1, B);
-	false ->
-	    amicable_pairs_below(A - 1, B)
-    end;
 amicable_pairs_below(A, B) ->
-    amicable_pairs_below(A - 1, B).
+    amicable_pairs_below(A, B, d(B)).
+
+amicable_pairs_below(1, B, _A) ->
+    amicable_pairs_below(B - 2, B - 1);
+amicable_pairs_below(A, B, A) ->
+    amicable_pairs_below(A, B, A, d(A));
+amicable_pairs_below(A, B, SB) ->
+    amicable_pairs_below(A - 1, B, SB).
+
+amicable_pairs_below(A, B, A, B) ->
+     io:format("A = ~p, B = ~p~n",[A, B]),
+     A + B + amicable_pairs_below(A - 1, B, A);
+amicable_pairs_below(A, B, A, _SA) ->
+     amicable_pairs_below(A - 1, B, A).
     
 solve() ->
     N = 10000,
-    %% Seq = lists:N
-    %% L = [{A, B} || A <- ]
-    amicable_pairs_below(N-1,N).
+    amicable_pairs_below(N).
