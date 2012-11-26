78863234ce3dbd8df1d27190534192acf3bc7db9
Jon <jon@powha.net>
2012-11-27 00:28:09 +0100

    Perimeter of triangle, list comprehension

--- e039.erl
+++ e039.erl
@@ -7,13 +7,31 @@
 % C > A
 % C > B
 
-
-
 solve() ->
-    A = ordsets:from_list(lists:seq(1, 1000)),
+    solve(1000).
+
+solve(1) ->
+    0; 
+solve(P) ->
+    A = lists:seq(2, P),
     B = A,
     C = A,
-    P = A,
-    propagate(A, B, C).
+    max(length(solve(A, B, C, P)), solve(P - 1)).
 
-propagate() ->
+    
+solve(A, B, C, P) ->
+    Sets = [{X, Y, Z} || X <- A, Y <- B, Z <- C,
+            X < Z,
+            Y < Z,
+            X < Y,
+            sum_prop(X, Y, Z, P),
+            pyth_prop(X, Y, Z)
+           ],
+    io:format("~p, ~p~n", [P, Sets]),
+    Sets.
+
+sum_prop(A, B, C, P) ->
+    A + B + C =:= P.
+
+pyth_prop(A, B, C) ->
+    A*A + B*B =:= C*C.
